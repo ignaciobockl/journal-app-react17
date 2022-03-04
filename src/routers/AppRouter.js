d6d301b62c83edc1b5a1d35cb80635f08c8b6054
Ignacio Bockl <ignaciobockl96@gmail.com>
2022-03-04 16:59:31 -0300

    login global

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
     BrowserRouter as Router,
     Switch,
     Route,
     Redirect
   } from "react-router-dom";
+import { useDispatch } from 'react-redux';
+
+import { firebase } from '../firebase/firebase-config';
+
+import { login } from '../actions/auth';
 
 import { JournalScreen } from '../components/journal/JournalScreen';
 
@@ -12,6 +17,36 @@ import { AuthRouter } from './AuthRouter';
 
 
 export const AppRouter = () => {
+  
+  const dispatch = useDispatch();
+
+  const [ checking, setChecking ] = useState( true );
+  const [ isLoggedIn, setIsLoggedIn ] = useState( false );
+
+  useEffect(() => {
+    
+    firebase.auth().onAuthStateChanged( (user) => {
+      
+      if ( user?.uid ) {
+        dispatch( login( user.uid, user.displayName ) );
+        setIsLoggedIn( true );
+      } else {
+        setIsLoggedIn( false );
+      }
+
+      setChecking( false );
+
+    });
+
+  }, [ dispatch, setChecking, setIsLoggedIn ] );
+
+  if ( checking ) {
+    return (
+      <h1>Espere...</h1>
+    )
+  }
+  
+
   return (
     <Router>
         <div>
